a1255d8aaa86d6cd423be429d598ed154c662833
André Santos <andre.santos@venkonetworks.com>
2024-07-25 16:06:34 -0300

    Ajustes no agw tools para incluir a probe.sh

--- probe.sh
+++ probe.sh
@@ -7,24 +7,24 @@ TIMEOUT=$1
 declare FAIL_GTPU=0
 declare FAIL_GTPBR=0
 
-set -x 
+set -x
 
 traffic_ongoing() {
     #captura pacotes a cada minuto
     packets_gtpu=$(timeout $TIMEOUT sudo tcpdump -i gtpu_sys_2152 2>&1 | grep -E "^0 packets captured")
     packets_gtpbr=$(timeout $TIMEOUT sudo tcpdump -i gtp_br0 -Q out 2>&1 | grep -E "^0 packets captured")
-} 
+}
 
-check_gtpu(){
+check_gtpu() {
     #verifica se captura pegou algo
     if [[ $packets_gtpu == "0 packets captured" ]]; then
         FAIL_GTPU=1
     else
         FAIL_GTPU=0
-    fi    
+    fi
 }
 
-check_gtpbr(){
+check_gtpbr() {
     #verifica se captura está zerada
     if [[ $packets_gtpbr == "0 packets captured" ]]; then
         FAIL_GTPBR=1
@@ -36,125 +36,124 @@ check_gtpbr(){
 dump_config() {
     # gera um dump do estado atual do host
     if [[ ! -f "$GET_CONFIGURATION_FILE" ]]; then
-      touch "$GET_CONFIGURATION_FILE"
+        touch "$GET_CONFIGURATION_FILE"
     fi
 
-    echo "Command: ping -c 10 10.0.2.100" >> "$GET_CONFIGURATION_FILE"
-    ping -c 10 10.0.2.100 >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: ping -c 10 10.0.2.100" >>"$GET_CONFIGURATION_FILE"
+    ping -c 10 10.0.2.100 >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
+    echo "Command: date" >>"$GET_CONFIGURATION_FILE"
+    date >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: date" >> "$GET_CONFIGURATION_FILE"
-    date >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: uptime" >>"$GET_CONFIGURATION_FILE"
+    uptime >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: uptime" >> "$GET_CONFIGURATION_FILE"
-    uptime >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: uname -a" >>"$GET_CONFIGURATION_FILE"
+    uname -a >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: uname -a" >> "$GET_CONFIGURATION_FILE"
-    uname -a >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: top -b -n 1" >>"$GET_CONFIGURATION_FILE"
+    top -b -n 1 >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: top -b -n 1" >> "$GET_CONFIGURATION_FILE"
-    top -b -n 1 >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: df -kh" >>"$GET_CONFIGURATION_FILE"
+    df -kh >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: df -kh" >> "$GET_CONFIGURATION_FILE"
-    df -kh >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: journalctl -u magma@mme --since \"10 min ago" >>"$GET_CONFIGURATION_FILE"
+    journalctl -u magma@mme --since "10 min ago" >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: journalctl -u magma@mme --since \"10 min ago" >> "$GET_CONFIGURATION_FILE"
-    journalctl -u magma@mme --since "10 min ago" >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: journalctl -u magma@pipelined --since \"10 min ago" >>"$GET_CONFIGURATION_FILE"
+    journalctl -u magma@pipelined --since "10 min ago" >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: journalctl -u magma@pipelined --since \"10 min ago" >> "$GET_CONFIGURATION_FILE"
-    journalctl -u magma@pipelined --since "10 min ago" >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: netstat -i" >>"$GET_CONFIGURATION_FILE"
+    netstat -i >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: netstat -i" >> "$GET_CONFIGURATION_FILE"
-    netstat -i >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: ovs-vsctl show" >>"$GET_CONFIGURATION_FILE"
+    ovs-vsctl show >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: ovs-vsctl show" >> "$GET_CONFIGURATION_FILE"
-    ovs-vsctl show >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: ovs-ofctl show gtp_br0" >>"$GET_CONFIGURATION_FILE"
+    ovs-ofctl show gtp_br0 >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: ovs-ofctl show gtp_br0" >> "$GET_CONFIGURATION_FILE"
-    ovs-ofctl show gtp_br0 >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: ovs-ofctl dump-flows gtp_br0" >>"$GET_CONFIGURATION_FILE"
+    ovs-ofctl dump-flows gtp_br0 >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: ovs-ofctl dump-flows gtp_br0" >> "$GET_CONFIGURATION_FILE"
-    ovs-ofctl dump-flows gtp_br0 >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: service magma@* status" >>"$GET_CONFIGURATION_FILE"
+    service magma@* status >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: service magma@* status" >> "$GET_CONFIGURATION_FILE"
-    service magma@* status >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: service sctpd status" >>"$GET_CONFIGURATION_FILE"
+    service sctpd status >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: service sctpd status" >> "$GET_CONFIGURATION_FILE"
-    service sctpd status >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: service openvswitch-switch status" >>"$GET_CONFIGURATION_FILE"
+    service openvswitch-switch status >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: service openvswitch-switch status" >> "$GET_CONFIGURATION_FILE"
-    service openvswitch-switch status >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: show_gateway_info.py" >>"$GET_CONFIGURATION_FILE"
+    show_gateway_info.py >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: show_gateway_info.py" >> "$GET_CONFIGURATION_FILE"
-    show_gateway_info.py >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: checkin_cli.py" >>"$GET_CONFIGURATION_FILE"
+    checkin_cli.py >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: checkin_cli.py" >> "$GET_CONFIGURATION_FILE"
-    checkin_cli.py >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: mobility_cli.py get_subscriber_table" >>"$GET_CONFIGURATION_FILE"
+    mobility_cli.py get_subscriber_table >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: mobility_cli.py get_subscriber_table" >> "$GET_CONFIGURATION_FILE"
-    mobility_cli.py get_subscriber_table >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: pipelined_cli.py debug display_flows" >>"$GET_CONFIGURATION_FILE"
+    pipelined_cli.py debug display_flows >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: pipelined_cli.py debug display_flows" >> "$GET_CONFIGURATION_FILE"
-    pipelined_cli.py debug display_flows >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: enodebd_cli.py get_all_status" >>"$GET_CONFIGURATION_FILE"
+    enodebd_cli.py get_all_status >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: enodebd_cli.py get_all_status" >> "$GET_CONFIGURATION_FILE"
-    enodebd_cli.py get_all_status >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: ip addr" >>"$GET_CONFIGURATION_FILE"
+    ip addr >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: ip addr" >> "$GET_CONFIGURATION_FILE"
-    ip addr >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: ping google.com -I eth0 -c 5" >>"$GET_CONFIGURATION_FILE"
+    ping google.com -I eth0 -c 5 >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: ping google.com -I eth0 -c 5" >> "$GET_CONFIGURATION_FILE"
-    ping google.com -I eth0 -c 5 >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: ip route" >>"$GET_CONFIGURATION_FILE"
+    ip route >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: ip route" >> "$GET_CONFIGURATION_FILE"
-    ip route >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: iptables -L" >>"$GET_CONFIGURATION_FILE"
+    iptables -L >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: iptables -L" >> "$GET_CONFIGURATION_FILE"
-    iptables -L >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: iptables -L -v -n" >>"$GET_CONFIGURATION_FILE"
+    iptables -L -v -n >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: iptables -L -v -n" >> "$GET_CONFIGURATION_FILE"
-    iptables -L -v -n >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: iptables -t nat -L -v -n" >>"$GET_CONFIGURATION_FILE"
+    iptables -t nat -L -v -n >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: iptables -t nat -L -v -n" >> "$GET_CONFIGURATION_FILE"
-    iptables -t nat -L -v -n >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: iptables -t mangle -L -v -n" >>"$GET_CONFIGURATION_FILE"
+    iptables -t mangle -L -v -n >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: iptables -t mangle -L -v -n" >> "$GET_CONFIGURATION_FILE"
-    iptables -t mangle -L -v -n >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: iptables -t raw -L -v -n" >>"$GET_CONFIGURATION_FILE"
+    iptables -t raw -L -v -n >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 
-    echo "Command: iptables -t raw -L -v -n" >> "$GET_CONFIGURATION_FILE"
-    iptables -t raw -L -v -n >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
-
-    echo "Command: iptables -t security -L -v -n" >> "$GET_CONFIGURATION_FILE"
-    iptables -t security -L -v -n >> "$GET_CONFIGURATION_FILE"
-    echo -e "\n" >> "$GET_CONFIGURATION_FILE"
+    echo "Command: iptables -t security -L -v -n" >>"$GET_CONFIGURATION_FILE"
+    iptables -t security -L -v -n >>"$GET_CONFIGURATION_FILE"
+    echo -e "\n" >>"$GET_CONFIGURATION_FILE"
 }
 
 send_notification() {
@@ -162,7 +161,7 @@ send_notification() {
     #A principio apenas envia um alerta
 
     PAYLOAD="payload_json={\"username\": \"Magma\", \"content\": \"ALERTA DE FALHA $1 EM $(date) NO CLIENTE $2\"}"
-    curl -F "$PAYLOAD" https://discord.com/api/webhooks/932746721358913607/x_STDYIfsEFUiTglwSkd1YJY9i5Jy5OOmq7WZx6fz79Mz0zBkaWtsg3XSPGeIf-dFu3G     
+    curl -F "$PAYLOAD" https://discord.com/api/webhooks/932746721358913607/x_STDYIfsEFUiTglwSkd1YJY9i5Jy5OOmq7WZx6fz79Mz0zBkaWtsg3XSPGeIf-dFu3G
 }
 
 while true; do
@@ -170,17 +169,16 @@ while true; do
     check_gtpu
     check_gtpbr
     if [[ $FAIL_GTPU -eq 1 ]]; then
-	send_notification "GTPU" $2
+        send_notification "GTPU" $2
     fi
     if [[ $FAIL_GTPBR -eq 1 ]]; then
-	send_notification "GTPBR" $2
+        send_notification "GTPBR" $2
     fi
     if [[ $FAIL_GTPU -eq 1 || $FAIL_GTPBR -eq 1 ]]; then
-    	dump_config
+        dump_config
         systemctl restart sctpd
-	#exit 0
+        #exit 0
     fi
     sleep 10
     echo "Rodando em $(date)"
 done
-
