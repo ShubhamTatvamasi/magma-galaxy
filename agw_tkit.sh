d3f0e1efa8fb8f6a491f50619ae53b35e19c92e9
André Santos <andre.santos@venkonetworks.com>
2025-01-17 10:34:41 -0300

    Tmux utility for magma debugging

---
+++ agw_tkit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SESH=0
+WNAME="Magma Toolkit"
+CMDPANE1="health_cli.py"
+CMDPANE2="mobility_cli.py get_subscriber_table"
+CMDPANE3="ping google.com"
+
+tmux has-session -t $SESH 2>/dev/null
+
+if [ $? != 0 ]; then
+	tmux new-session -d -s $SESH -n "$WNAME"
+	tmux split-window -h
+	tmux split-window -v
+	tmux send-keys -t $SESH:"$WNAME".0 "$CMDPANE1" C-m
+	tmux send-keys -t $SESH:"$WNAME".1 "$CMDPANE2" C-m
+	tmux send-keys -t $SESH:"$WNAME".2 "$CMDPANE3" C-m
+	tmux attach -t 0	
+fi
+
+if [ $? -eq 0 ]; then
+    tmux attach -t $SESH
+fi
+
+exit 0
+
